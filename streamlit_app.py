0c5f7e5e18313b5970228dd3809505a7607c2eaa
Jonas Pena <jonasp.dev@gmail.com>
2023-10-02 11:48:45 -0400

    Update streamlit_app.py
    changes to text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,4 @@
 import streamlit
 
-streamlit.title('My parents new healthy dinner. This is an update.')
+streamlit.header('My parents new healthy dinner. This is an update.')
+streamlit.text('New text new text new text')
